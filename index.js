95090a4836b98783331e6ebf84f26e7a34657497
GabrielleMoreno <127845040+GabrielleMoreno@users.noreply.github.com>
2024-09-12 01:07:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const parte1= "Olá, tudo bem?";
+const primeiroNome ="Gabrielle";
+const sobrenome ="Moreno";
+const idade= 17;
+const altura= 1.58;
+const massa= 58;
+const cidadeNatal="são josé dos pinhais";
+const meuDia= "eu trabalho no período da manhã, estudo para enem e vestibular de tarde e de noite vou para a escola";
+const sobreMim= "Eu gosto de dançar, fazer títulos e amo sair com os meus amigos";
+
+console.log(parte1);
+console.log(primeiroNome);
+console.log(sobrenome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidadeNatal);
+console.log(meuDia);
+console.log(sobreMim);
